2918ff1ed055ddfbadb2bdefa6252e635a59f01e
tgadeliya <tgadeliya@gmail.com>
2020-04-13 20:56:20 +0200

    models implemented

--- logistic_regression_scratch/src/models.py
+++ logistic_regression_scratch/src/models.py
@@ -2,76 +2,93 @@ import numpy as np
 
 class LogisticRegressionBinary:
     
-    def __init__(self, C=1, learning_rate=0.05, alpha=0.5,
-                 max_iter = 10, eps = 10e-6,
-                 batch_size = 32, shuffle=False):
+    def __init__(self, C=1, 
+                 learning_rate_init=0.05,
+                 alpha=0.5,
+                 weight_decay=0.9,
+                 max_iter = 10,
+                 batch_size = 32,random_state = 25,
+                 tol = 0.001,
+                 logging = False,
+                 num_classes=1):
+        
+        self.rand_gen = np.random.RandomState(random_state) # set generator specific to model object
         
         self.C = C
-        self.lr = learning_rate;
+        self.lr_init = learning_rate_init
+        
         self.alpha_momentum = alpha
-        
-        
         self.max_iter = max_iter
-        self.eps = eps
         
         self.batch_size = batch_size
-        self.shuffle = shuffle
         
+        self.tol = tol
+        self.weight_decay = weight_decay
         self.w = None;
         self.loss_history = []
+        self.num_classes = num_classes
+        
         
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         
-        self.X = X#self.add_bias(X)
-        self.y = y[:]
-        self.N, self.d = self.X.shape
-        
-        self.w = np.random.randn(self.d, 1)
-        self.h = np.zeros((self.d,1))
-        
-        for e in range(1, self.max_iter+1):
+        if (self.w is None): # First fit start
+            self.X = self.add_bias(X)
+            self.y = y[:]
+            self.N, self.d = self.X.shape
+            self.batch_iter = np.ceil(self.N / self.batch_size).astype("int")
             
-            self.train()
-            
-            #val_improv = 
-            #if val_improv < self.eps and (e > 1):
-            #    print("Stopping criteria is met!")
-            #    break
+            self.w = self.rand_gen.randn(self.d, self.num_classes)
+            self.h = np.zeros_like(self.w)
         
+        self.lr = self.lr_init
+        self.epoch = 0 
+        self.no_epoch_improv = 0
+        
+        self.train()
+            
     def train(self) -> None:
-        "Perform one epoch train"
-        
-        batch_idx = list(range(self.N))
-        
-        if self.shuffle:
-            np.random.shuffle(batch_idx)
-        
-        batch_iter = np.ceil(self.N/self.batch_size)
-        avg_loss = 0.0
-
-        for idx in range(int(batch_iter)):
-            batch = batch_idx[idx*self.batch_size:(idx+1)*self.batch_size]
-            avg_batch_loss, grad_batch = self.calc_loss_and_grad(self.X[batch], self.y[batch])
+        for e in range(1, self.max_iter+1): # If stopping criteria isn't met, train till max_iter
+            batch_idx = self.rand_gen.permutation(self.N) # shuffle indexes every epoch
             
-            self.optimizer_step_on_batch(grad_batch)
-            avg_loss += avg_batch_loss
-        
-        self.loss_history.append(avg_loss/batch_iter) # Mean example loss   
+            for i in range(self.batch_iter):
+                batch = batch_idx[i*self.batch_size:(i+1)*self.batch_size]
+                grad_batch = self.calc_grad(self.X[batch], self.y[batch])
+                self.optimizer_step_on_batch(grad_batch)
+            
+            self.epoch += 1
+            train_loss = self.loss(self.X, self.y)
+            train_acc = self.evaluate(self.X, self.y)
+            
+            print(f"Epoch {e}: train_loss: {train_loss}, train_acc: {train_acc}")
+            print("_____________________________")
+            
+            self.loss_history.append(train_loss)
+            
+            self.lr_scheduler()
+            
+            self.no_epoch_improv = (self.no_epoch_improv + 1) if (e > 2 and (self.loss_history[-1] - self.loss_history[-2])<self.tol) else 0
+            
+            if (self.no_epoch_improv > 3):
+                print("Stopping criteria is met!")
+                break
     
-    def calc_loss_and_grad(self, X:np.ndarray, y:np.ndarray) -> tuple:
+    def loss(self, X_batch, y_batch):
+        lin_comb = np.dot(X_batch, self.w) # [N,d] X [d,] -> [N,]
         
-        lin_comb = np.dot(X, self.w) # [N,d] X [d,] -> [N,]
+        loss_batch = np.log(1+np.exp(-1*y_batch*lin_comb))
+        regularization = (0.5 * self.C) * (self.w[1:] ** 2).sum()
         
-        loss = -np.log(1+np.exp(-1*y*lin_comb)) +  self.C * np.linalg.norm(self.w, ord=2)
-        grad_batch = (self.sigmoid(lin_comb) - y) * X + self.C*2*self.w.reshape(1,-1)
-        #print(f"loss :{loss}")
-        #print("Gradients along rows :", grad_batch.sum(axis=1))
+        return np.mean(loss_batch, axis=0) + regularization
         
-        return np.mean(loss), grad_batch
+    def calc_grad(self, X:np.ndarray, y:np.ndarray) -> np.ndarray:
+        grad_batch = (self.sigmoid(np.dot(X, self.w)) - y) * X + self.C*self.w.reshape(1,-1)
+        return grad_batch
     
+    def lr_scheduler(self):
+        self.lr = (1/(1+self.weight_decay*self.epoch))*self.lr_init 
     
     def optimizer_step_on_batch(self, grad_batch : np.ndarray) -> None:    
-        w_grad = np.mean(grad_batch, axis=0).reshape(-1,1)
+        w_grad = np.mean(grad_batch, axis=0).reshape(-1,self.num_classes)
         
         self.h = self.h * self.alpha_momentum + self.lr * w_grad
         self.w -= self.h 
@@ -83,134 +100,66 @@ class LogisticRegressionBinary:
         return np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
     
     def predict(self, X: np.ndarray) -> np.ndarray:        
-        out = np.dot(X, self.w)
-        #out = np.dot(self.add_bias(X), self.w)
-        probs = self.sigmoid(out)
+        probs = self.predict_proba(X)
         return np.where(probs>0.5, 1, -1)
     
-    
     def predict_proba(self, X: np.ndarray) -> np.ndarray:        
-        out = np.dot(X, self.w)
-        #out = np.dot(self.add_bias(X), self.w)
+        out = np.dot(self.add_bias(X), self.w)
         probs = self.sigmoid(out)
         return probs 
+  
+    def evaluate(self, X, y_true):
+        "Calculate accuracy between epoch"
+        probs = self.sigmoid(np.dot(X, self.w))
+        y_pred = np.where(probs>0.5, 1, -1)
+        acc = sum(y_pred == y_true)/ len(y_true)
+        return acc
     
-    
-    
-    @staticmethod
-    def evaluate(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        acc = (y_true == y_pred)
-        return sum(acc)/len(y_true)
-
-
-    
-    
-                               
-class LogisticRegressionMultiClass(LogisticRegression):
-    def get_loss_with_grad(self, X:np.ndarray, y:np.ndarray) -> tuple:
-        pass
-    
-    def predict_prob(self, X):
-        return np.dot(X,self.w)
-
-    
-    
-class LogisticRegressionSGD(LogisticRegressionBinary):
-    
-    def __init__(self, C=1, learning_rate=0.05, alpha=0.5, max_iter = 10, eps = 10e-6, shuffle=False):
-        
-        self.C = C
-        self.lr = learning_rate;
-        self.alpha_momentum = alpha
-        
-        
-        self.max_iter = max_iter
-        self.eps = eps
-        
-        self.shuffle = shuffle
-        
-        self.w = None;
-        self.loss_history = []
-        
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        
-        self.X = self.add_bias(X)
-        self.y = y[:]
-        self.N, self.d = self.X.shape
-        
-        self.w = np.random.randn(self.d, 1)
-        self.h = np.zeros((self.d,1))
-        
-        for e in range(1, self.max_iter+1):
-            
-            self.train()
-            
-            #val_improv = 
-            #if val_improv < self.eps and (e > 1):
-            #    print("Stopping criteria is met!")
-            #    break
-        
-    def train(self) -> None:
-        "Perform one epoch train"
-        
-        batch_idx = list(range(self.N))
-        
-        if self.shuffle:
-            np.random.shuffle(batch_idx)
-        
-        batch_iter = np.ceil(self.N/self.batch_size)
-        avg_loss = 0.0
-
-        for idx in range(int(batch_iter)):
-            batch = batch_idx[idx*self.batch_size:(idx+1)*self.batch_size]
-            avg_batch_loss, grad_batch = self.calc_loss_and_grad(self.X[batch], self.y[batch])
-            
-            self.optimizer_step_on_batch(grad_batch)
-            avg_loss += avg_batch_loss
-        
-        self.loss_history.append(avg_loss/batch_iter) # Mean example loss   
-    
-    def calc_loss_and_grad(self, X:np.ndarray, y:np.ndarray) -> tuple:
-        
-        lin_comb = np.dot(X, self.w) # [N,d] X [d,] -> [N,]
-        
-        loss = -np.log(1+np.exp(-1*y*lin_comb)) +  self.C * np.linalg.norm(self.w, ord=2)
-        grad_batch = (self.sigmoid(lin_comb) - y) * X + self.C*2*self.w.reshape(1,-1)
-        #print(f"loss :{loss}")
-        #print("Gradients along rows :", grad_batch.sum(axis=1))
-        
-        return np.mean(loss), grad_batch
-    
-    
-    def optimizer_step_on_batch(self, grad_batch : np.ndarray) -> None:    
-        w_grad = np.mean(grad_batch, axis=0).reshape(-1,1)
-        
-        self.h = self.h * self.alpha_momentum + self.lr * w_grad
-        self.w -= self.h 
-    
-    def sigmoid(self, x: np.ndarray) -> np.ndarray:
-        return 1/(1+np.exp(-1*x))
-    
-    def add_bias(self, X) :
-        return np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
+class LogisticRegressionMulti(LogisticRegressionBinary):
     
     def predict(self, X: np.ndarray) -> np.ndarray:        
-        out = np.dot(X, self.w)
-        #out = np.dot(self.add_bias(X), self.w)
-        probs = self.sigmoid(out)
-        return np.where(probs>0.5, 1, -1)
-    
+        return self.predict_proba(X).argmax(axis=1)
     
     def predict_proba(self, X: np.ndarray) -> np.ndarray:        
-        out = np.dot(X, self.w)
-        #out = np.dot(self.add_bias(X), self.w)
-        probs = self.sigmoid(out)
+        out = np.dot(self.add_bias(X), self.w)
+        probs = self.softmax(out)
         return probs 
     
+    def softmax(self, x):
+        "Softmax over batch"
+        x -= x.max(axis=1).reshape(-1,1)
+        x_exp = np.exp(x)
+        return x_exp / x_exp.sum(axis=1).reshape(-1, 1)
+        
+    def loss(self, X, y):
+        y_shift = y - 2
+        probs = self.softmax(np.dot(X, self.w))
+        loss = -np.log(probs[range(X.shape[0]),y_shift.ravel()])
+        regularization = (0.5 * self.C) * (self.w[1:,:] ** 2).sum()
+        
+        return np.mean(loss) + regularization
     
+    def calc_grad(self,X:np.ndarray, y:np.ndarray) -> tuple:
+        y_shift = y-2 # Shift over deleted classes
+        
+        lin_comb = np.dot(X, self.w)
+        probs = self.softmax(lin_comb)
+        
+        probs[range(X.shape[0]), y_shift.ravel()] = - (1 - probs[range(X.shape[0]), y_shift.ravel()]) 
+        grad_batch = X[:,:,np.newaxis] * probs[:,np.newaxis,:]   # [batch_size, [d,1] ] * [batch_size, [1,C]] -> [batch_size, d, c]         
+        return grad_batch 
     
-    @staticmethod
-    def evaluate(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        acc = (y_true == y_pred)
-        return sum(acc)/len(y_true)
-
+    def predict(self, X: np.ndarray) -> np.ndarray:        
+        probs = self.predict_proba(X)
+        return probs.argmax(axis=1)+2
+    
+    def predict_proba(self, X: np.ndarray) -> np.ndarray:        
+        out = np.dot(self.add_bias(X), self.w)
+        probs = self.softmax(out)
+        return probs 
+    
+    def evaluate(self, X, y_true):
+        "Calculate accuracy between epoch"
+        y_pred = self.predict(X[:,1:])
+        acc = sum(y_pred == y_true.ravel())/ len(y_true)
+        return acc
